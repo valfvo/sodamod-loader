f7e677a2b75e5ebd03cc12a843e89c0232ee2bee
valfvo <49768521+valfvo@users.noreply.github.com>
2021-06-14 22:44:00 +0200

    Add Makefile

--- SodaMod/ModLoader/ModLoader.cs
+++ SodaMod/ModLoader/ModLoader.cs
@@ -1,9 +1,16 @@
-﻿using System;
+using System;
+using System.IO;
 
 namespace SodaMod.ModLoader
 {
-    public class ModLoader
+    public static class ModLoader
     {
+        public static readonly string BaseDirectory = 
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SodaModLoader");
+
+        public static readonly string ModDirectory = 
+            Path.Combine(BaseDirectory, "Mods");
+
         static void Main(string[] args)
         {
             // TODO
