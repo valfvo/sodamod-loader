52c885d5a2724b272d541149a72da7e6d00aac5f
valfvo <49768521+valfvo@users.noreply.github.com>
2021-07-06 20:49:48 +0200

    Cleanup

--- SodaMod/ModLoader/Patches.cs
+++ SodaMod/ModLoader/Patches.cs
@@ -10,6 +10,9 @@ namespace SodaMod.ModLoader
     [HarmonyPatch(typeof(UIMenuTitle), "Awake")]
     public static class UIMenuTitle_Awake_Patch
     {
+        private static Matrix4x4 translationMatrix;
+        private static Matrix4x4 scaleMatrix;
+
         public static void Postfix(UIMenuTitle __instance)
         {
             Sprite smlOptionsSprite = Main.services.bundleManager.Get<Sprite>("ui", "btn_options");
@@ -25,38 +28,21 @@ namespace SodaMod.ModLoader
             float scaleFactor = root.GetComponent<CanvasScaler>().scaleFactor;
             Vector2 documentSize = root.rect.size;
 
-            Vector4 screenPos = new Vector4(330, 0, 0, 1);
+            Vector4 screenPosition = new Vector4(330, 0, 0, 1);
 
             float w = documentSize.x;
             float h = documentSize.y;
-            Matrix4x4 translation = Matrix4x4.Translate(new Vector4(w / 2, h / 2, 0, 1) * -1);
-            Matrix4x4 scale = Matrix4x4.Scale(new Vector4(1, -1, 1, 1) / scaleFactor);
+            translationMatrix = Matrix4x4.Translate(new Vector4(w / 2, h / 2, 0, 1) * -1);
+            scaleMatrix = Matrix4x4.Scale(new Vector4(1, -1, 1, 1) / scaleFactor);
 
             // translate -> scale
-            SodaLogger.WriteLine((scale * translation).ToString());
-            SodaLogger.WriteLine((scale * translation * screenPos).ToString());
-            Vector3 localPos = scale * translation * screenPos;
+            Vector3 localPosition = scaleMatrix * translationMatrix * screenPosition;
+            rt.localPosition = localPosition;
 
-            // Vector2 localPos = new Vector2(screenPos.x - documentSize.x / 2, -(screenPos.y - documentSize.y / 2)) / scaleFactor;
-            rt.localPosition = localPos;
-            // Vector2 localPos;
-            // Camera uiCamera = Main.services.uiCamera.GetCam();
-            // RectTransform parentRt = rt.parent as RectTransform;
-            // RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            //     parentRt, screenPos, uiCamera, out localPos
-            // );
-            // SodaLogger.WriteLine("local scale: " + parentRt.GetComponent<CanvasScaler>().scaleFactor);
             SodaLogger.WriteLine("Scale factor: " + scaleFactor);
-            // rt.localPosition = localPos + rt.rect.size / 2;
-            // rt.anchorMin = rt.anchorMax = new Vector2(0, 0);
-            // rt.anchoredPosition = rt.rect.size / 2;
             SodaLogger.WriteLine("Document size: " + documentSize.ToString());
-            // Main.services.uiBuilder.AnchorElementToBottomRight(
-            //     (RectTransform) smlOptionsButton.transform, new Vector2(0f, 80f)
-            // );
 
-            // SodaLogger.WriteLine("Scale factor: " + GameContext.canvasScaleFactor);
-            WriteTransformTree(smlOptionsButton.transform.root);
+            WriteTransformTree(root);
         }
 
         private static void SmlOptionsClickListener()
@@ -73,27 +59,15 @@ namespace SodaMod.ModLoader
                 string indent = new string(' ', depth);
 
                 Camera uiCamera = Main.services.uiCamera.GetCam();
-                Vector2 localPosition = transform.localPosition;
-                Vector2 position = transform.position;
-
-                Vector3[] localCornerList = new Vector3[4];
-                (transform as RectTransform).GetWorldCorners(localCornerList);
-                string localCorners = "";
-                foreach (Vector3 localCorner in localCornerList)
-                {
-                    localCorners += "(" + localCorner.x + ", " + localCorner.y + ") ";
-                }
-                // Rect rect = ((RectTransform) transform).rect;
-                // Vector2 bottomLeft = uiCamera.WorldToScreenPoint(rect.position);
-                // Vector2 bottomLeft = rect.position;
-                // Vector2 center = uiCamera.WorldToScreenPoint(rect.center);
-                // Vector2 center = rect.center;
-                // Vector2 dimensions = new Vector2(rect.width, rect.height);
+                Vector4 localPosition = rt.localPosition;
+                localPosition.w = 1;
+                Vector2 clientPosition = scaleMatrix * translationMatrix * localPosition;
 
                 SodaLogger.WriteLine(
                     indent + transform.name + ": "
                     // + localCorners
                     + "local: " + localPosition + ", "
+                    + "client: " + clientPosition + ", "
                     // + "rt: " + rt.anchorMin + ", "
                     // + "bottomLeft: " + bottomLeft.ToString() + ", "
                     // + "center: " + center.ToString() + ", "
